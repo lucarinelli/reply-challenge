dc921dd610ffa144d119e610215ab3c45e30b663
federica <federica.fanigliulo97@gmail.com>
2018-03-15 12:00:40 +0100

    Data structure header file

---
+++ datastructure.c
@@ -0,0 +1,5 @@
+//
+// Created by federica on 15/03/18.
+//
+
+#include "datastructure.h"
---
+++ datastructure.h
@@ -0,0 +1,8 @@
+//
+// Created by federica on 15/03/18.
+//
+
+#ifndef PROJECT_DATASTRUCTURE_H
+#define PROJECT_DATASTRUCTURE_H
+
+#endif //PROJECT_DATASTRUCTURE_H
