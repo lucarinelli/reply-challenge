0c4c2a4ae0dd8c184478da4f58e4713ce0e9f7c2
federica <federica.fanigliulo97@gmail.com>
2018-03-15 18:07:20 +0100

    Math functions

--- output.h
+++ output.h
@@ -6,5 +6,9 @@
 #define REPLY_CHALLENGE_OUTPUT_H
 
 #include "datastructure.h"
+#include "math_functions.h"
+//define output data structure
+//generate an output data structure
+//minimize score
 
 #endif //REPLY_CHALLENGE_OUTPUT_H
