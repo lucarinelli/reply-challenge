b4ee393a49cf32a33f3db85df9f4afb97f2e2162
Luca Rinelli <rinelliluca@gmail.com>
2018-03-15 17:08:55 +0100

    Data structure

--- datastructure.h
+++ datastructure.h
@@ -5,4 +5,34 @@
 #ifndef PROJECT_DATASTRUCTURE_H
 #define PROJECT_DATASTRUCTURE_H
 
+typedef struct handle handle;
+typedef struct provider provider;
+typedef struct project project;
+typedef struct region region;
+
+struct handle{
+    int V_n_prov, S_n_serv, C_n_coun, P_n_proj;
+    provider *providers[];
+    project *projects[];
+    char **services;
+};
+
+struct provider{
+    region *regions[];
+    int n_regions;
+};
+
+struct project{
+    int base_penalty;
+    int country;//reference intera? change to pointer?
+    int units_per_service[];
+};
+
+struct region{
+    int available_packages;
+    float package_unit_cost;
+    int units_per_serv[];
+    int latency_per_coun[];
+};
+
 #endif //PROJECT_DATASTRUCTURE_H
